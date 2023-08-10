ee38b4d5e9850066471936ec16643049aac69ac7
sahar7assan <sahar.h.ahmed@gmail.com>
2023-08-10 07:08:06 -0700

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char mystring[] = "Programming is like building a multilingual puzzle";
+
+	puts(mystring);
+	return (0);
+}

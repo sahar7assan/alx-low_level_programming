6447994fb8847f818e7023fe74d937cf2098f388
sahar7assan <sahar.h.ahmed@gmail.com>
2023-08-15 13:03:50 -0700

    add tow number

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* add - add tow numbers
+*@n: int number
+*@e: int number2
+* Return: tow number (Success)
+*
+*/
+int add(int n, int e)
+{
+	int result;
+
+	result = n + e;
+	return (result);
+}

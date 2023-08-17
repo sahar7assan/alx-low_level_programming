505d6f8313c54f996bbf524e88d1e3cb1a4038d7
sahar7assan <sahar.h.ahmed@gmail.com>
2023-08-17 09:36:00 -0700

    print straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,12 +8,12 @@ void print_line(int n)
 {
 	int i;
 	
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
 	for (i = 1 ; i <= n ; i++)
 	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
 		_putchar(95);
 	}
 	_putchar('\n');

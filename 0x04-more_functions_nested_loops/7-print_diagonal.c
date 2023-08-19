5c5db7fbb4ab215ca8cabe32cc5aed6b36a7532e
sahar7assan <sahar.h.ahmed@gmail.com>
2023-08-19 13:53:37 -0700

    draws a diagonal line on the term

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,14 +8,23 @@
 void print_diagonal(int n)
 {
 	int i;
+	int j;
 
-	for (i = 1 ; i <= n ; i++)
-	{
-		if (n <= 0)
+		for (i = 0 ; i < n ; i++)
 		{
+			if (n <= 0)
+			{
+				_putchar('\n');
+			}
+			for (j = 0 ; j < i ; j++)
+				_putchar(' ');
+
+			_putchar('\\');
+
+			if (i == n - 1)
+				continue;
+
 			_putchar('\n');
 		}
-		_putchar(92);
-	}
-	_putchar('\n');
+		_putchar('\n');
 }

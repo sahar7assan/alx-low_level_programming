758219a85ed25e7e2219a6c942e77aabd0692a44
sahar7assan <sahar.h.ahmed@gmail.com>
2023-08-29 11:22:34 -0700

    search string

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,18 +7,19 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i;
+	int i = 0;
 	int j;
 
-	for (i = 0 ; s[i] >= '\0' ; i++)
+	while (*s)
 	{
-		for (j = 0 ; accept[j] >= '\0' ; j++)
+		for (j = 0 ; accept[j] != '\0' ; j++)
 		{
-			if (s[i] == accept[j])
+			if (*s == accept[j])
 			{
 				return (s + i);
 			}
 		}
+		s++;
 	}
 	return (NULL);
 

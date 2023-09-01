6f913b53d41ae571ca02c2da0fe0e83b5ded77c5
sahar7assan <sahar.h.ahmed@gmail.com>
2023-09-01 06:19:37 -0700

    find prim number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,16 +11,16 @@ int _prime_number(int n, int i)
 		return (1);
 	if (n % i == 0 && i > 0)
 		return (0);
-	return (_prime(n, i - 1));
+	return (_prime_number(n, i - 1));
 }
 /**
- * _is_prime_number - prime number
+ * is_prime_number - prime number
  * @n: input int
  * Return: int factorial
  */
-int _is_prime_number(int n)
+int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return (_prime(n, n - 1));
+	return (_prime_number(n, n - 1));
 }

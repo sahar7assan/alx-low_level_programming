44b16a5a91698aa98d2a5babf01a9a7561168c14
sahar7assan <sahar.h.ahmed@gmail.com>
2023-09-04 14:45:09 -0700

    duplicate string in new location

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup -  function duplicate string in new memory location
+ * @str: string to be copied
+ *
+ * Return: pointer to memory location
+ */
+char *_strdup(char *str)
+{
+	char *duplicate;
+	int length = 0;
+	int i = 0;
+	int j = 0;
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		length++;
+	}
+	duplicate  = malloc(length * sizeof(char));
+
+	if (duplicate == NULL)
+	{
+		return (NULL);
+	}
+
+	while (j < i)
+	{
+		duplicate[j] = str[j];
+		j++;
+	}
+	return (duplicate);
+}

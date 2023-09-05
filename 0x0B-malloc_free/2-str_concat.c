a280f367dedb5038b88a181c5cbca37558025ca9
sahar7assan <sahar.h.ahmed@gmail.com>
2023-09-05 14:01:56 -0700

    concatnate tow string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,25 +14,20 @@ char *str_concat(char *s1, char *s2)
 	int count = 0;
 	int i, j, c;
 
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
 	for (i = 0 ; s1[i] != '\0' ; i++)
 		;
 	for (j = 0; s2[j] != '\0'; j++)
 		;
 
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
-
 	newStr = malloc((i + j + 1) * sizeof(char));
 
 	if (newStr == NULL)
 	{
-		free(newStr);
 		return (NULL);
 	}
 

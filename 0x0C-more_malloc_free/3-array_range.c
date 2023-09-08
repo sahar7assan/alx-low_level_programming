d95a7302092d6d12d23ac0199e99ff23630f40e2
sahar7assan <sahar.h.ahmed@gmail.com>
2023-09-08 15:21:24 -0700

    array rang

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * @min: input min value
+ * @max: input max value
+ * @n: number of by
+ *
+ * Return: pointer to orderd array
+ */
+int *array_range(int min, int max)
+{
+	int *arRang;
+	int length, i;
+
+	if (min > max)
+		return (NULL);
+	length = (max - min) + 1;
+	arRang = malloc (length * sizeof(int));
+
+	if (arRang == NULL)
+		return (NULL);
+
+	for (i = 0 ; min <= max ; min++, i++)
+	{
+		arRang[i] = min;
+	}
+	return (arRang);
+}

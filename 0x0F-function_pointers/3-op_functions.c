0e27aa042161afc3aa136875095b4df207d14ca4
sahar7assan <sahar.h.ahmed@gmail.com>
2023-09-13 11:20:32 -0700

    simple opreations

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,65 +1,66 @@
 #include "3-calc.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+
 /**
- * op_add - function add tow integer
- * @a: an integer
- * @b: an integer
- *
- * Return:sum
+ * op_add - function that sums 2 numbers.
+ * @a: number1
+ * @b: number2
+ * Return: the sum of both numbers.
  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+
 /**
- * op_sub - function return sub of tow integer
- * @a: an integer
- * @b: an integer
- *
- * Return:sub
+ * op_sub - function that subtractions two numbers.
+ * @a: number 1.
+ * @b: number 2.
+ * Return: the subtractions of both numbers.
  */
+
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
 /**
- * op_mul - function return mul of tow integer
- * @a: an integer
- * @b: an integer
- *
- * Return:mul
+ * op_mul - function that multiply two numbers.
+ * @a: number 1.
+ * @b: number 2.
+ * Return: the multiply of both numbers.
  */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+
 /**
- * op_div - function return div of tow integer
- * @a: an integer
- * @b: an integer
- *
- * Return:div
+ * op_div - function that divide two numbers.
+ * @a: number 1.
+ * @b: number 2.
+ * Return: the division of both numbers.
  */
 int op_div(int a, int b)
 {
-	if (a == 0)
+	if (b <= 0 || b > a)
 	{
 		printf("Error\n");
 		exit(100);
 	}
 	return (a / b);
 }
+
 /**
- * op_mod - function return mod of tow integer
- * @a: an integer
- * @b: an integer
- *
- * Return:mod
+ * op_mod - function that find the module between two numbers.
+ * @a: number 1.
+ * @b: number 2.
+ * Return: the module of both numbers.
  */
 int op_mod(int a, int b)
 {
-	if (a == 0)
+	if (b <= 0 || b > a)
 	{
 		printf("Error\n");
 		exit(100);

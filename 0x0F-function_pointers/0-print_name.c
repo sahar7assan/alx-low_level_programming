08f790bb771eacbc860f61700822e5d61cba01f6
sahar7assan <sahar.h.ahmed@gmail.com>
2023-09-13 11:29:41 -0700

    edit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -8,16 +8,14 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	void print_name(char *name, void (*f)(char *))
+	if (name == NULL)
 	{
-		if (name == NULL)
-		{
-			return;
-		}
-		if (f == NULL)
-		{
-			return;
-		}
-		f(name);
+		return;
 	}
+	if (f == NULL)
+	{
+		return;
+	}
+	f(name);
 }
+

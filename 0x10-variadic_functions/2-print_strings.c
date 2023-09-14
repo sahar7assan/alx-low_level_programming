329e8061c4ff4b567f40acdeb9cae425d861a7c7
sahar7assan <sahar.h.ahmed@gmail.com>
2023-09-14 10:09:17 -0700

    print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("nil");
 		}
 		printf("%s", str);
-		if (separator != NULL && index != (n - 1))
+		if (separator != NULL && index < (n - 1))
 		{
 			printf("%s", separator);
 		}

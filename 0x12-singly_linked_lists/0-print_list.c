2bf38104c73dc66fc338cfa652d9ffe87d89fcaa
sahar7assan <sahar.h.ahmed@gmail.com>
2023-09-22 04:59:38 -0700

    print lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,17 +10,19 @@ size_t print_list(const list_t *h)
 {
 	int i = 0;
 
-	for (; h != NULL; h = h->next, i++)
+	while (h != NULL)
 	{
 		if (h->str == NULL)
 		{
 			printf("[0] (nil)\n");
+			h = h->next;
 		}
 		else
 		{
 			printf("[%d] %s\n", h->len, h->str);
 			h = h->next;
 		}
+		i++;
 	}
 	return (i);
 }

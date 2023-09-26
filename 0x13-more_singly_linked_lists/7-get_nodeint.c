bc9a3d84823d51102b3ee0b040d626cdc6523a0e
sahar7assan <sahar.h.ahmed@gmail.com>
2023-09-26 15:15:30 -0700

    get node edit

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,8 +10,8 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int count;
-	
-	for (count = 0 ; count < index && head != NULL ; count++)
+
+	for (count = 0; count < index && head != NULL; count++)
 	{
 		head = head->next;
 	}

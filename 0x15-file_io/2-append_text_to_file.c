cbde2aafff04fa6a683eb3b8acca0d49283ae9fa
sahar7assan <sahar.h.ahmed@gmail.com>
2023-10-02 13:59:09 -0700

    append text

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,7 +13,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	newfile = open(filename, O_WRONLY | O_APPEND);
+	newfile = open(filename, O_RDWR | O_APPEND);
 	if (newfile == -1)
 	{
 		return (-1);

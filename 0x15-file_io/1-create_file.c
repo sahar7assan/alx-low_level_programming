a505bd019c42309cfd1d7d4036565a201d277dd2
sahar7assan <sahar.h.ahmed@gmail.com>
2023-10-02 12:23:01 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	newfile = open(filename, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
+	newfile = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (newfile == -1)
 	{
 		return (-1);

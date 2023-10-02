8b92063f7bed29e36ebe09276d3ebf0c936197b8
sahar7assan <sahar.h.ahmed@gmail.com>
2023-10-02 12:31:48 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,15 +18,9 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	if (text_content == NULL)
-	{
-		if (close(newfile) == -1)
-		{
-			return (-1);
-		}
-		else
-			return (1);
-	}
+	if (!text_content)
+		text_content = "";
+	
 	text = _strlen(text_content);
 
 	fw = write(STDOUT_FILENO, text_content, text);

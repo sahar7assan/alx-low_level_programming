3850c34e95f541c68410abb2381d6d176017aa28
sahar7assan <sahar.h.ahmed@gmail.com>
2023-11-23 00:11:13 -0800

    binary to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * binary_to_uint - convert binary
+ * @b: pointer
+ * Return: int value
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int bb = 0;
+
+	while (b && *b)
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+
+		bb = bb << 1;
+		bb = bb | (*b - '0');
+		b++;
+	}
+	return (bb);
+}
+

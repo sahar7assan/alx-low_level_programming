0ebbbd9ead25f68a524b47e6d2f634ad760ad8c6
sahar7assan <sahar.h.ahmed@gmail.com>
2023-11-27 09:18:03 -0800

    edit append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,44 +7,30 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int newfile;
-	int fw, text;
+	int f;
+	int newletter;
+	int rwr;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	newfile = open(filename, O_RDWR | O_APPEND);
-	if (newfile == -1)
+	f = open(filename, O_WRONLY | O_APPEND);
+
+	if (f  == -1)
+		return (-1);
+
+	if (text_content)
 	{
-		return (-1);
-	}
-	if (text_content == NULL)
-	{
-		return (-1);
-	}
-	text = _strlen(text_content);
+		for (newletter = 0; text_content[newletter]; newletter++)
+			;
 
-	fw = write(newfile, text_content, text);
-	if (fw == -1)
-	{
-		return (-1);
+		rwr = write(f, text_content, newletter);
+
+		if (rwr == -1)
+			return (-1);
 	}
 
-	close(newfile);
+	close(f);
+
 	return (1);
 }
-
-/**
- * _strlen - funtion count len of char.
- * @s: count string.
- * Return: string.
- */
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (s[len] != '\0')
-		len++;
-	return (len);
-}
-
